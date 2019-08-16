b996f7b67de84c45bcbb8be560ac5216237c76d7
playwithbear <goldspinky@hotmail.com>
2019-08-16 10:29:39 +0100

    Updates

---
+++ precache-manifest.eeac47dc6fc5dacb85183904441ccf91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770d04ef5d0cd435e6f7b4456f57aaa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a90df7a0457787f8d8d5",
+    "url": "/static/js/2.f6d8a373.chunk.js"
+  },
+  {
+    "revision": "c9a6ced6027d15a4bd75",
+    "url": "/static/js/main.1911a0bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e7fafca8f8337fb88bdc11c61d49fd20",
+    "url": "/static/media/Whats-in-standard.e7fafca8.png"
+  }
+]);
